5017f2e960ef17b792bb644746c0b6f8e8fa08c9
BINDHARAN <bindharan885@gmail.com>
2022-03-28 12:17:15 +0530

    updated

--- routes/transactions.js
+++ routes/transactions.js
@@ -27,7 +27,7 @@ router.get('/', async (req, res) => {
 
 //READ TRANSACTION BY ID
 
-router.get("/edit/:id", async (req, res) => {
+router.get("/:id", async (req, res) => {
   const { id } = req.params;
   const result = await getTransactionById(id);
   result
